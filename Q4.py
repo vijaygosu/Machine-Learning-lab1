beba7a307ddad8a1fbf7140313f7693872d29025
vijaygosu <155065878+vijaygosu@users.noreply.github.com>
2024-03-11 23:36:45 +0530

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -1,13 +1,13 @@
 def max_occur(string):
-    count={}
-    for i in string:
-            count[i]=count.get(i,0)+1
+    count = {}  
+    for char in string:  # Iterate through each character
+        count[char] = count.get(char, 0) + 1  
 
-    max_char=max(count,key=count.get)
-    max_count=count[max_char]
+    max_char = max(count, key=count.get)  # Find the character with the maximum count
+    max_count = count[max_char]  # Get the count of the maximum occurring character
 
     return max_char, max_count
 
-string="hippopotamus"
-char,count= max_occur(string)
-print("the most occurring character is ",char," with count",count)
+string = "hippopotamus"  # Sample string
+char, count = max_occur(string)   
+print("the most occurring character is ", char, " with count", count)  
